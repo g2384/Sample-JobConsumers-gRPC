7bff7b5cdee3f997e5ee464388bc944295874221
g2384 <g2384@users.noreply.github.com>
2023-03-10 11:36:12 +0000

    init

---
+++ src/JobService.Components/ConvertVideoJobConsumer.cs
@@ -0,0 +1,34 @@
+﻿namespace JobService.Components
+{
+    using System;
+    using System.Threading.Tasks;
+    using MassTransit;
+    using Microsoft.Extensions.Logging;
+
+
+    public class ConvertVideoJobConsumer :
+        IJobConsumer<ConvertVideo>
+    {
+        readonly ILogger<ConvertVideoJobConsumer> _logger;
+
+        public ConvertVideoJobConsumer(ILogger<ConvertVideoJobConsumer> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task Run(JobContext<ConvertVideo> context)
+        {
+            var rng = new Random();
+
+            var variance = TimeSpan.FromMilliseconds(rng.Next(8399, 28377));
+
+            _logger.LogInformation("Converting Video: {GroupId} {Path}", context.Job.GroupId, context.Job.Path);
+            
+            await Task.Delay(variance);
+
+            await context.Publish<VideoConverted>(context.Job);
+            
+            _logger.LogInformation("Converted Video: {GroupId} {Path}", context.Job.GroupId, context.Job.Path);
+        }
+    }
+}
